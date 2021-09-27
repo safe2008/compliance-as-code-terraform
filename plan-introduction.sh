a2b030ef4d6ef5c1bfbab27fd67d3438af8d515a
safe2008 <boriphuth.sae@truedigital.com>
2021-09-27 19:36:24 +0700

    Update

--- plan-introduction.sh
+++
@@ -1,4 +0,0 @@
-terraform -chdir=introduction init
-terraform -chdir=introduction plan --out tfplan.binary
-terraform -chdir=introduction show -json tfplan.binary > introduction/tfplan.json
-checkov -f introduction/tfplan.json
